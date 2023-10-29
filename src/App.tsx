956dfa94321dfc9ba67275b43437afab9e18b50f
Michael O'Connor <m.k.oconnor53@gmail.com>
2023-10-28 20:43:20 -0500

    initial giphy input

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div
+        style={{
+          width: "250px",
+          // height: "100px",
+          paddingBottom: "75%",
+          position: "relative",
+        }}
+      >
+        <iframe
+          src="https://giphy.com/embed/Ju7l5y9osyymQ"
+          width="100%"
+          height="100%"
+          style={{ position: "absolute" }}
+          className="giphy-embed"
+          allowFullScreen
+        ></iframe>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        <a href="https://giphy.com/gifs/rick-astley-Ju7l5y9osyymQ"></a>
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
